6ecd479759f8344ab3d8ddc3a2cb99dd9cb95e7d
Yohei Kokubo <yhei.register@gmail.com>
2023-11-16 08:30:36 +0900

    文言修正

---
+++ app/nabeatu.py
@@ -0,0 +1,14 @@
+class Nabeatu:
+    def __init__(self, num: int):
+        if type(num) != int:
+            raise ValueError("数字を入れてください")
+        if num <= 0:
+            raise ValueError("1以上の数値を入れてください")
+        self.num = num
+
+    def call(self) -> str:
+        if self.num % 3 == 0:
+            return f"{self.num}(アホ)"
+        if  "3" in str(self.num):
+            return f"{self.num}(アホ)"
+        return str(self.num)
